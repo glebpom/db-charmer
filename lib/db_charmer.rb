4dedd0a6753ef5d7033711eeb76d9c44b5e6baac
Alexey Kovyrin <alexey@kovyrin.net>
2009-09-30 10:12:35 +0800

    Change multi-db migrations to works with rails 2.2

--- lib/db_charmer.rb
+++ lib/db_charmer.rb
@@ -59,7 +59,7 @@ require 'db_charmer/multi_db_migrations'
 require 'db_charmer/multi_db_proxy'
 
 # Enable multi-db migrations
-ActiveRecord::Migration.extend(DbCharmer::MultiDbMigrations::ClassMethods)
+ActiveRecord::Migration.extend(DbCharmer::MultiDbMigrations)
 
 # Enable the magic
 ActiveRecord::Base.extend(DbCharmer::DbMagic::ClassMethods)
